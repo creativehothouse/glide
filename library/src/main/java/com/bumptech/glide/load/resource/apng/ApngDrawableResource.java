a135ff70a786a458b4d22eab6977eccbf7674038
edwin <imnotproz@gmail.com>
2017-10-25 17:04:56 +0200

    implement initialize on apng drawable resource

--- library/src/main/java/com/bumptech/glide/load/resource/apng/ApngDrawableResource.java
+++ library/src/main/java/com/bumptech/glide/load/resource/apng/ApngDrawableResource.java
@@ -1,12 +1,15 @@
 package com.bumptech.glide.load.resource.apng;
 
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import com.bumptech.glide.load.engine.Initializable;
 import com.bumptech.glide.load.engine.Resource;
 import com.bumptech.glide.load.resource.gif.GifDrawable;
 
 /**
  * A resource wrapping an {@link GifDrawable}.
  */
-public class ApngDrawableResource implements Resource<ApngDrawable> {
+public class ApngDrawableResource implements Resource<ApngDrawable>, Initializable {
 
   private final ApngDrawable apngDrawable;
 
@@ -28,5 +31,12 @@ public class ApngDrawableResource implements Resource<ApngDrawable> {
 
   @Override public void recycle() {
     apngDrawable.stop();
+  }
+
+  @Override public void initialize() {
+    Drawable firstFrame = apngDrawable.getFrame(0);
+    if (firstFrame instanceof BitmapDrawable) {
+      ((BitmapDrawable) firstFrame).getBitmap().prepareToDraw();
+    }
   }
 }
